f123de144db9f10d272b419adb188fa15cd03fbf
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-06-25 14:01:30 +0300

    Add final variable PORT in class App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,7 +11,8 @@ public class App {
 // postgresql://page_analyzer_db_oxdu_user:4wyudCOvKC1lp37X9F676oSjYKzAZjid@dpg-cpt9e4qju9rs73ant040-a/page_analyzer_db_oxdu
 // postgresql://page_analyzer_db_oxdu_user:4wyudCOvKC1lp37X9F676oSjYKzAZjid@dpg-cpt9e4qju9rs73ant040-a.oregon-postgres.render.com/page_analyzer_db_oxdu
 
-    private static final int PORT = 8080;
+    private static final int DEFAULT_PORT = 8080;
+    private static final int PORT = 5432;
 
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
