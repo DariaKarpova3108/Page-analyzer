49853b75672f2bdc8eadbeb041d4ae9b375f1d89
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-07-01 21:23:08 +0300

    add comments for future work and add method readResources in App.class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,14 +11,20 @@ import hexlet.code.repository.BaseRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class App {
 
     public static int getPort() {
-       String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
     }
 
@@ -46,14 +52,16 @@ public class App {
         var dataSource = new HikariDataSource(hikariConfig);
         BaseRepository.dataSource = dataSource;
 
+        var sql = readResources("schema.sql");
 
+        log.info(sql);
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
         app.get(NamedRoutes.rootPath(), RootController::index);
-        app.post(NamedRoutes.urlsPath(),UrlsController::create);
+        app.post(NamedRoutes.urlsPath(), UrlsController::create);
         app.get(NamedRoutes.urlsPath(), UrlsController::showAllUrls);
         app.get(NamedRoutes.urlsPathWithChecks(), UrlsController::showAllUrlsWithChecks);
         app.get(NamedRoutes.urlPath("{id}"), UrlsController::showUrl);
@@ -61,8 +69,16 @@ public class App {
         return app;
     }
 
+    private static String readResources(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        assert inputStream != null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
     public static void main(String[] args) throws SQLException, IOException {
-            var app = getApp();
-            app.start(getPort());
+        var app = getApp();
+        app.start(getPort());
     }
 }
